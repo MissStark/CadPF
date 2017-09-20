9b22b70e07ab0b9bd3fd94178049415f67fb6840
Sarah Ellen <30556338+MissStark@users.noreply.github.com>
2017-09-19 22:15:58 -0300

    package model

---
+++ Cadastro.java
@@ -0,0 +1,88 @@
+package model;
+
+public class Cadastro {
+	String nome, endereco, bairro, cep, cidade, estado, telefone, celular, rg, cpf;
+	char sexo;
+	
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getEnd(){
+		return endereco;
+	}
+	public void setEnd(String endereco){
+		this.endereco = endereco;
+	}
+	
+	public String getBairro(){
+		return bairro;
+	}
+	public void setBairro(String bairro){
+		this.bairro = bairro;
+	}
+	
+	public String getCep(){
+		return cep;
+	}
+	public void setCep(String cep){
+		this.cep = cep;
+	}
+	
+	public String getCid(){
+		return cidade;
+	}
+	public void setCid(String cidade){
+		this.cidade = cidade;
+	}
+	
+	public String getEst(){
+		return estado;
+	}
+	public void setEst(String estado){
+		this.estado = estado;
+	}
+	
+	public String getTel(){
+		return telefone;
+	}
+	public void setTel(String telefone){
+		this.telefone = telefone;
+	}
+	
+	public String getCel(){
+		return celular;
+	}
+	public void setCel(String celular){
+		this.celular = celular;
+	}
+	
+	public String getRg(){
+		return telefone;
+	}
+	public void setRg(String rg){
+		this.rg = rg;
+	}
+	
+	public String getCpf(){
+		return telefone;
+	}
+	public void setCpf(String cpf){
+		this.cpf = cpf;
+	}
+	
+	public char getSexo(){
+		return sexo;
+	}
+	public void setSexo(char sexo){
+		this.sexo = sexo;
+	}
+	
+}
+		
+	
+	
+	
